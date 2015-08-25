9cfc47702708ddb364e552f274f50ff4f86a1188
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-08-25 13:08:50 -0700

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # registry-benchmark
-Simple tool to benchmark docker-registry
+
+Simple tool to benchmark docker-registry using `docker pull` command.
+
+## Usage
+
+```
+registry-benchmark <image[:tag]> <iterations>
+```
+
+Make sure your shell already has the environment variables such as
+`DOCKER_HOST`, `DOCKER_CERT_DIR` (if applicable) set and Docker client
+installed.
+
+The program will write logs to stderr, and duration of each `docker pull`
+(in seconds) to the stdout.
+
+## Author
+
+Ahmet Alp Balkan
+
+## License
+
+See [LICENSE](LICENSE).
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+func init() {
+	log.SetOutput(os.Stderr)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatalf("Usage: %s <image[:tag]> <iterations>", os.Args[0])
+	}
+
+	image := os.Args[1]
+	iter, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal("Error parsing iterations count: %v", err)
+	}
+
+	// image := "node:0.10.31-onbuild"
+	log.Printf("Image: %s", image)
+	for i := 0; i < iter; i++ {
+		log.Printf("Run %d:", i)
+
+		// Clear the image with `docker rmi`
+		if err := rmi(image); err != nil {
+			log.Fatal(err)
+		}
+
+		start := time.Now()
+		if err := pull(image); err != nil {
+			log.Fatal(err)
+		}
+		d := time.Since(start)
+		fmt.Printf("%d\n", int64(d.Seconds()))
+	}
+
+}
+
+func rmi(image string) error {
+	// See if image exists
+	if _, err := dockerCmd("inspect", image); err != nil {
+		// image doesn't exist (or some other failure which will come up below)
+		return nil
+	}
+
+	if b, err := dockerCmd("rmi", "-f", image); err != nil {
+		return fmt.Errorf("rmi failed: %v: %q", err, string(b))
+	}
+	return nil
+}
+
+func pull(image string) error {
+	if b, err := dockerCmd("pull", image); err != nil {
+		return fmt.Errorf("pull failed: %v: %q", err, string(b))
+	}
+	return nil
+}
+
+func dockerCmd(args ...string) ([]byte, error) {
+	if os.Getenv("DOCKER_CERT_PATH") != "" {
+		args = append([]string{"--tls"}, args...)
+	}
+	cmd := exec.Command("docker", args...)
+	return cmd.CombinedOutput()
+}
